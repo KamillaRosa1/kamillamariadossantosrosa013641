094ce74c2450c38723a52c8cf0d50c38f25a7368
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-26 11:11:51 -0400

    :sparkles: Criação da entidade Artista no model

---
+++ src/main/java/com/projeto/api_artistas/model/Artista.java
@@ -0,0 +1,29 @@
+package com.projeto.api_artistas.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "artistas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Artista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String tipo;
+
+    @ManyToMany(mappedBy = "artistas")
+    private Set<Album> albuns = new HashSet<>();
+    
+}
