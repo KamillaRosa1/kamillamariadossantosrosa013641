719f481ba91fa71bb0f529bf1c7175f50e75e9f8
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-27 12:27:34 -0400

    :sparkles: Foi criada a interface ArtistaRepository para operações CRUD via Spring Data JPA

---
+++ src/main/java/com/projeto/api_artistas/repository/ArtistaRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.api_artistas.repository;
+
+import com.projeto.api_artistas.model.Artista;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+}
