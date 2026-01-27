d0f6a5a67c7bbeb52adcdbb32538f330f8297a1f
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-27 19:03:12 -0400

    :sparkles: Foi criado o DTO AlbumDTO para transferência de dados de álbuns e seus artistas entre camadas

---
+++ src/main/java/com/projeto/api_artistas/dto/AlbumDTO.java
@@ -0,0 +1,16 @@
+package com.projeto.api_artistas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlbumDTO {
+    private Long id;
+    private String titulo;
+    private String imagemCapaUrl;
+    private Set<ArtistaDTO> artistas;
+}
