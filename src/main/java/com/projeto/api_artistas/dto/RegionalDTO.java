409177780fa76f93338104319ba1d7345866d718
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-29 15:47:57 -0400

    :sparkles: criação do RegionalDTO para transporte de dados da API externa

---
+++ src/main/java/com/projeto/api_artistas/dto/RegionalDTO.java
@@ -0,0 +1,13 @@
+package com.projeto.api_artistas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegionalDTO {
+    private Long id;
+    private String nome;
+}
