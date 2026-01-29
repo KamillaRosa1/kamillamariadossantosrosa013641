8bb009e56dc31a12b9ad1b8beaddf6306b77be9e
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-29 14:52:46 -0400

    :recycle: atualização da entidade Album com relacionamento N:N e suporte a imagem de capa

--- src/main/java/com/projeto/api_artistas/model/Album.java
+++ src/main/java/com/projeto/api_artistas/model/Album.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import java.util.HashSet;
 import java.util.Set;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -13,10 +12,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Album {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(nullable = false)
