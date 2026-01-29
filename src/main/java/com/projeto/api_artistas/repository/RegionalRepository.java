3d6457ce3a11fb33df3b4c7a6305f4e9921ac76e
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-29 15:08:18 -0400

    :sparkles: criação do RegionalRepository

---
+++ src/main/java/com/projeto/api_artistas/repository/RegionalRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.api_artistas.repository;
+
+import com.projeto.api_artistas.model.Regional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RegionalRepository extends JpaRepository<Regional, Long> {
+    // Ajuste Sênior: Para sincronizar, precisamos achar o registro por ID externo
+    // independente de estar ativo ou não, para evitar duplicatas.
+    Optional<Regional> findByIdExterno(Long idExterno);
+}
