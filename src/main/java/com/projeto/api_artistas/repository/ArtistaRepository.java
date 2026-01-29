a6e2da5b5f9c4f55f885ab56162909e8dc1ad791
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-29 15:04:51 -0400

    :recycle: atualização do ArtistaRepository com consultas por nome e tipo paginadas

--- src/main/java/com/projeto/api_artistas/repository/ArtistaRepository.java
+++ src/main/java/com/projeto/api_artistas/repository/ArtistaRepository.java
@@ -1,9 +1,16 @@
 package com.projeto.api_artistas.repository;
 
 import com.projeto.api_artistas.model.Artista;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+    // Atende Requisito 'f': Busca por nome com paginação/ordenação
+    Page<Artista> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+
+    // Atende Requisito 'e': Busca por tipo
+    Page<Artista> findByTipoIgnoreCase(String tipo, Pageable pageable);
 }
