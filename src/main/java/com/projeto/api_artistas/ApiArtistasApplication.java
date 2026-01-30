d28ae3b374569fbfbf14342799e3680f8bf4251b
Kamilla Rosa <kamillamsrosa@gmail.com>
2026-01-30 16:32:45 -0400

    :recycle: atualização da classe principal

--- src/main/java/com/projeto/api_artistas/ApiArtistasApplication.java
+++ src/main/java/com/projeto/api_artistas/ApiArtistasApplication.java
@@ -2,12 +2,16 @@ package com.projeto.api_artistas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/* @EnableScheduling ativa o suporte para a execução periódica 
+ * do RegionalService.sincronizar().
+ */
 @SpringBootApplication
+@EnableScheduling
 public class ApiArtistasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiArtistasApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiArtistasApplication.class, args);
+    }
 }
